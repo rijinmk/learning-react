c6a26f24137bd4bd1cdd01983e2390466f2f2928
Rijin Mk <rijin.mk9@gmail.com>
2021-04-16 03:15:03 +0400

    Remove state for the Ingredient list

--- rijin/hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ rijin/hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList'; 
 import Search from './Search';
 
 function Ingredients() {
+
+  const [ingredients, setIngredients] = useState([]); 
+
+  const addIngredientHandler = (ingredient) => {
+    console.log(ingredient);
+    setIngredients((prevIngredients) => {
+      let appendedIngredients = [...prevIngredients, { id: Math.floor(Math.random() * 999999), ...ingredient }];
+      console.log(appendedIngredients);
+      return appendedIngredients;
+    }); 
+  }
+
+  const removeIngredientItemHandler = (ingredientID) => {
+    ingredients.forEach((e, i) => {
+      if (e.id === ingredientID){
+        ingredients.splice(i, 1); 
+      }
+    }); 
+    setIngredients([...ingredients]); 
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm addIngredients={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        {<IngredientList onRemoveItem={removeIngredientItemHandler} ingredients={ingredients} />}
       </section>
     </div>
   );
