ed5de158b00654674bd7c5f8f2b43b7dd3bdaf05
Rijin Mk <rijin.mk9@gmail.com>
2021-04-16 14:34:59 +0400

    useCallback and useRef

--- rijin/hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ rijin/hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList'; 
@@ -6,12 +6,34 @@ import Search from './Search';
 
 function Ingredients() {
 
+  let url = "https://react-burger-builder-9bc18-default-rtdb.firebaseio.com/ingredients.json";
+
   const [ingredients, setIngredients] = useState([]); 
 
-  const addIngredientHandler = (ingredient) => {
-    console.log(ingredient);
+  useEffect(() => {
+    (async () => {
+      let ingredientsData = await fetch(url);
+      ingredientsData = await ingredientsData.json();
+      let _ = []; 
+      if ( ingredientsData ){
+        Object.keys(ingredientsData).forEach((e) => {
+          ingredientsData[e].id = e; 
+          _.push(ingredientsData[e]);
+        }); 
+        setIngredients(_); 
+      }
+      console.log("From useEffect: ", ingredientsData);
+    })(); 
+    return () => {}
+  }, []); 
+
+  const addIngredientHandler = async (ingredient) => {
+    console.log(JSON.stringify(ingredients));
+    let jsonData = await fetch(url, { method: "POST", body: JSON.stringify(ingredient)});
+    jsonData = await jsonData.json(); 
+    console.log(jsonData); 
     setIngredients((prevIngredients) => {
-      let appendedIngredients = [...prevIngredients, { id: Math.floor(Math.random() * 999999), ...ingredient }];
+      let appendedIngredients = [...prevIngredients, { id: jsonData.name, ...ingredient }];
       console.log(appendedIngredients);
       return appendedIngredients;
     }); 
--- rijin/hooks-01-starting-project/src/components/Ingredients/Search.js
+++ rijin/hooks-01-starting-project/src/components/Ingredients/Search.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+
+  let url = "https://react-burger-builder-9bc18-default-rtdb.firebaseio.com/ingredients.json";
+
+  const [enteredFilter, setEnteredFilter] = useState(''); 
+  const inputRef = useRef(); 
+
+  useEffect(() => {
+    // console.log("Use effect is triggered"); 
+    let timeOut = setTimeout(async() => {
+      console.log("---------------------------------------"); 
+      console.log("enteredFilter", "inputRef.current.value");
+      console.log(enteredFilter, inputRef.current.value);
+      console.log("---------------------------------------");
+
+      if (enteredFilter === inputRef.current.value){
+        let q = enteredFilter.length===0 ? '' : `?orderBy="title"&equalsTo="${enteredFilter}"`;
+        console.log("From useEffect: ", url + q);
+        let jsonData = await fetch(url + q);
+        jsonData = await jsonData.json();
+        console.log(jsonData);
+      }
+    }, 1000); 
+
+    return () => {
+      clearTimeout(timeOut); 
+    }
+
+
+  }, [enteredFilter]);
+
+  const searchChangeHandler = (e) => {
+    setEnteredFilter(e.target.value); 
+  }
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input ref={inputRef} type="text" onChange={(e) => {searchChangeHandler(e)}} />
         </div>
       </Card>
     </section>
