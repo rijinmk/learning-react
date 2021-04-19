25e09124747ccd56fb80e7e3ba4fec88a2d4e3ea
Rijin Mk <rijin.mk9@gmail.com>
2021-04-19 04:05:43 +0400

    Search functionality init

--- rijin/doggie-walker/src/Components/Search/Search.js
+++ rijin/doggie-walker/src/Components/Search/Search.js
@@ -1,24 +1,40 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 export const Search = () => {
 
+    const [breed, setBreed] = useState(''); 
+    const searchRef = useRef(); 
+
     useEffect(() => {
-        (async () => {
-            let dogs = await fetch(
-                'https://api.thedogapi.com/v1/breeds?attach_breed=0', 
-                { 
-                    method: "GET", 
-                    "x-api-key": "27b47624-9780-4abe-bbb3-d8b516137750"
-                },
-            );
-            dogs = await dogs.json();
-            console.log("[Dog.js]: ", dogs);
-        })()
-    }, []); 
+        console.log(breed); 
+        let c = setTimeout(() => {
+            (async () => {
+                let dogs = await fetch(
+                    `https://api.thedogapi.com/v1/breeds/search?q=${breed}`,
+                    { 
+                        method: "GET", 
+                        "x-api-key": "27b47624-9780-4abe-bbb3-d8b516137750"
+                    },
+                );
+                dogs = await dogs.json();
+                console.clear(); 
+                console.log("[Dog.js]: ", dogs);
+            })()
+        }, 500); 
+
+        return () => {
+            clearTimeout(c); 
+        }
+
+    }, [breed]);
+
+    const searchHandler = () => {
+        setBreed(searchRef.current.value);
+    }
 
     return (
         <div>
-            Search + AutoSuggest
+            <input type="text" ref={searchRef} onChange={searchHandler} placeholder="Search Breed..."/>
         </div>
     )
 }
